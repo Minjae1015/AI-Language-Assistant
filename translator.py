c1d5b610800e1dd22d6f919b2d16c116796672cf
Minjae <kangminjae101015@gmail.com>
2026-07-10 14:16:14 +0200

    간단하게 언어 선택 기능 및 오류 대처

--- translator.py
+++ translator.py
@@ -1,24 +1,61 @@
 from deep_translator import GoogleTranslator
 
-text = input("번역할 문장을 입력하세요: ")
+#언어 목록
+languages = {
+    "1": "한국어",
+    "2": "영어",
+    "3": "일본어",
+    "4": "중국어(간체)",
+    "5": "프랑스어",
+    "6": "독일어",
+    "7": "스페인어"
+}
 
-print("\n사용 가능한 언어")
-print("ko = 한국어")
-print("en = 영어")
-print("ja = 일본어")
-print("zh-CN = 중국어(간체)")
-print("fr = 프랑스어")
-print("de = 독일어")
-print("es = 스페인어")
+lang_codes = {
+    "한국어": "ko",
+    "영어": "en",
+    "일본어": "ja",
+    "중국어(간체)": "zh-CN",
+    "프랑스어": "fr",
+    "독일어": "de",
+    "스페인어": "es"
+}
 
-source = input("원본 언어를 입력하세요 (자동 감지는 auto): ")
-target = input("번역할 언어를 입력하세요: ")
+#원본 언어 선택
+print("\n \n 원본 언어를 선택하세요.")
+for num, name in languages.items():
+    print(f"[{num}] {name}")
+print("[8] 자동 감지")
 
-translated = GoogleTranslator(
-    source=source,
-    target=target
-).translate(text)
+source_choice = input("입력 (1~8): ")
 
-print("번역 결과입니다:")
+#결과 언어 선택
+print("\n \n번역할 언어를 선택하세요.")
+for num, name in languages.items():
+    print(f"[{num}] {name}")
 
-print(translated)
+target_choice = input("입력 (1~7): ")
+
+#번역 문장 입력
+text = input("\n \n 번역할 문장을 입력하세요")
+
+if source_choice == "8":
+    source = "auto"
+else:
+    source = lang_codes[languages[source_choice]]
+
+target = lang_codes[languages[target_choice]]
+
+#번역 결과
+try:
+    translated = GoogleTranslator(
+        source=source,
+        target=target
+    ).translate(text)
+
+    print("\n \n 번역 결과입니다:")
+    print(translated)
+
+#오류
+except Exception:
+    print("\n \n 번역 중 오류가 발생했습니다.")
