710078bc27a73bb622738e38cfe01a451b406291
Minjae <kangminjae101015@gmail.com>
2026-07-13 12:44:26 +0200

    오류 부분 대응  업그레이드

--- translator.py
+++ translator.py
@@ -1,6 +1,6 @@
 from deep_translator import GoogleTranslator
 
-#언어 목록
+# 사용할 언어
 languages = {
     "1": "한국어",
     "2": "영어",
@@ -21,23 +21,41 @@ lang_codes = {
     "스페인어": "es"
 }
 
-#원본 언어 선택
-print("\n \n 원본 언어를 선택하세요.")
-for num, name in languages.items():
-    print(f"[{num}] {name}")
-print("[8] 자동 감지")
+# 원본 언어
+while True:
+    print("\n \n 원본 언어를 선택하세요:")
+    for num, name in languages.items():
+        print(f"[{num}] {name}")
+    print("[8] 자동 감지")
 
-source_choice = input("입력 (1~8): ")
+    source_choice = input("번호 입력: ")
 
-#결과 언어 선택
-print("\n \n번역할 언어를 선택하세요.")
-for num, name in languages.items():
-    print(f"[{num}] {name}")
+    if source_choice in languages or source_choice == "8":
+        break
 
-target_choice = input("입력 (1~7): ")
+    print("다시 입력해주세요.")
 
-#번역 문장 입력
-text = input("\n \n 번역할 문장을 입력하세요:")
+# 번역할 언어
+while True:
+    print("\n \n 번역할 언어를 선택하세요:")
+    for num, name in languages.items():
+        print(f"[{num}] {name}")
+
+    target_choice = input("번호 입력: ")
+
+    if target_choice in languages:
+        break
+
+    print("다시 입력해주세요.")
+
+# 번역할 문장
+while True:
+    text = input("\n \n 번역할 문장을 입력하세요: ")
+
+    if text.strip():
+        break
+
+    print("문장을 입력해주세요:")
 
 if source_choice == "8":
     source = "auto"
@@ -46,16 +64,10 @@ else:
 
 target = lang_codes[languages[target_choice]]
 
-#번역 결과
 try:
-    translated = GoogleTranslator(
-        source=source,
-        target=target
-    ).translate(text)
-
-    print("\n \n 번역 결과입니다:")
+    translated = GoogleTranslator(source=source, target=target).translate(text)
+    print("\n \n 번역 결과")
     print(translated)
 
-#오류
-except Exception:
-    print("\n \n 번역 중 오류가 발생했습니다")
+except:
+    print("오류가 발생했습니다.")
